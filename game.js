9090c231883ec795f4226d4c04e6fa2a3b69d0e6
Zac Arnold <videozac@gmail.com>
2020-05-01 14:37:29 +1200

    debug

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    click(dots[i]).addEventListener('contextmenu', makeBlue)
+    
   }
 }
 
@@ -23,11 +25,11 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
-function makeBlue (evt){
+function makeBlue (evt) {
   evt.target.classList.toggle('blue')
   updateCounts()
-  )
 }
+
 // CREATE FUNCTION hide HERE
 
 function updateCounts () {
